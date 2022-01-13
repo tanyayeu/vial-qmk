ab87eca3c3efb3abc2ae70b960a3b05bfd266719
tanyayeu <tanyayeu@gmail.com>
2022-01-12 18:03:21 -0800

    qaz vial

---
+++ keyboards/qaz/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x31, 0xF5, 0xD8, 0x5E, 0x89, 0x78, 0x19, 0x5F}
+
+#define IGNORE_MOD_TAP_INTERRUPT
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {1, 2}
